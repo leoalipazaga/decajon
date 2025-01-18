3ccfcc76fb36964df7fb72a6294b17ecb6802412
Leonardo Alipazaga <leonardoalipazagab@gmail.com>
2025-01-17 23:14:43 -0500

    feat: add brotli compression for static files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import svelte from '@astrojs/svelte'
+import compressor from 'astro-compressor'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [
+    tailwind(),
+    svelte(),
+    compressor({
+      fileExtensions: ['.svg', '.css', '.js', '.cjs', '.mjs'],
+    }),
+  ],
   redirects: {
     '/recipes': '/recipes/bloody-mary',
   },
